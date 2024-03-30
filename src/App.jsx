801ea0bc481e16aded9c8868ab07ad5687dc8830
CR7sewey <miguelmotacarvalho@gmail.com>
2024-03-30 16:28:35 +0000

    Creating functional components - Header, Content and Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Content from './Content'
+import Footer from './Footer'
+import Header from './Header'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <main>
-        Hello World!
-      </main>
-      
+      <Header />
+      <Content />
+      <Footer />
     </>
   )
 }
---
+++ src/Footer.jsx
@@ -0,0 +1,14 @@
+const Footer = () => {
+
+    return (
+      <footer>
+        <h2>
+          Footer!
+        </h2>
+        
+      </footer>
+    )
+  }
+  
+  export default Footer
+  
