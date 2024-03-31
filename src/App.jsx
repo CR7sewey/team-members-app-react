cee5b2c199f74b10aa69561e5b81acca9c59a47d
CR7sewey <miguelmotacarvalho@gmail.com>
2024-03-31 11:21:20 +0100

    Employees hooks and functions to App, Header and Footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,42 @@ import './App.css'
 import Content from './Content'
 import Footer from './Footer'
 import Header from './Header'
+import { useState } from 'react'
+import { data as dados } from "./assets/data/initialData"
+import Employees from './Employees'
+
+const data = [...dados];
 
 function App() {
 
+  const [selectedTeam, setSelectedTeam] = useState("TeamA");
+
+  const [employees, setEmployees] = useState([...data]);    
+    
+  const handleTeamSelectionChange = (e) => {
+      console.log(e.target.value);
+
+      setSelectedTeam(e.target.value);
+      console.log(selectedTeam);
+    }
+    
+  function handleEmployeeCardClick(e) {
+      const transformedEmployees = employees.map((employee) => employee.id === parseInt(e.currentTarget.id)
+      ? (employee.teamName === selectedTeam)?{...employee, teamName: ''}:{...employee, teamName: selectedTeam}:employee);
+      console.log(transformedEmployees);
+      setEmployees(transformedEmployees);
+    }
+
   return (
     <>
-      <Header />
-      <Content />
+      <Header selectedTeam={selectedTeam} 
+      teamMemberCount={employees.filter((employee) => employee.teamName === selectedTeam).length} />
+      <Employees 
+      employees={employees}
+      selectedTeam={selectedTeam}
+      handleEmployeeCardClick={handleEmployeeCardClick}
+      handleTeamSelectionChange={handleTeamSelectionChange}
+       />
       <Footer />
     </>
   )
--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,11 +1,12 @@
 const Footer = () => {
-
+    let today = new Date();
     return (
-      <footer>
-        <h2>
-          Footer!
-        </h2>
-        
+      <footer className="container">
+        <div className="row justify-content-center mt-3 mb-4">  
+          <div className="col-8">
+            <h5>{today.toLocaleDateString()}</h5>
+          </div>
+        </div>   
       </footer>
     )
   }
