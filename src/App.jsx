674ca63f84a6c6d4f130edddc5f81fc41090fbd6
CR7sewey <miguelmotacarvalho@gmail.com>
2024-03-31 11:33:11 +0100

    Data to localstorage (localstorage and useEffect)

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import Content from './Content'
 import Footer from './Footer'
 import Header from './Header'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { data as dados } from "./assets/data/initialData"
 import Employees from './Employees'
 
@@ -10,9 +10,19 @@ const data = [...dados];
 
 function App() {
 
-  const [selectedTeam, setSelectedTeam] = useState("TeamA");
+  // buscar data da local storage
+  const [selectedTeam, setSelectedTeam] = useState(JSON.parse(localStorage.getItem('selectedTeam')) || "TeamA");
 
-  const [employees, setEmployees] = useState([...data]);    
+  const [employees, setEmployees] = useState(JSON.parse(localStorage.getItem('employeeList')) || [...data]);    
+  useEffect(() => {
+    localStorage.setItem('employeeList', JSON.stringify(employees))
+  }, [employees]); // only executes when the state of employees changes ([employees])
+
+  useEffect(() => {
+    localStorage.setItem('selectedTeam', JSON.stringify(selectedTeam))
+  }, [selectedTeam]); // only executes when the state of employees changes ([employees])
+
+
     
   const handleTeamSelectionChange = (e) => {
       console.log(e.target.value);
